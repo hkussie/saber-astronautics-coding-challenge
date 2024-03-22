316b363425611aa52b0025028c6fbf481f0f1134
Harrison Kussie <harrison@vapi.ai>
2024-03-21 19:49:55 -0700

    remove test file

--- sample.js
+++
@@ -1,5 +0,0 @@
-const { config } = require('./src/config/index');
-
-console.log('FTP Host:', config.ftpHost);
-console.log('FTP User:', config.ftpUser);
-console.log('FTP Password:', config.ftpPassword);
